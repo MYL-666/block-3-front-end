4b620b7b2de3eb130d9235238a6c1a459c2fc205
MYL-666 <l39601331@student.ua92.ac.uk>
2025-02-05 01:02:46 +0000

    update main

--- main/index/index-main.js
+++ main/index/index-main.js
@@ -124,4 +124,19 @@ window.onload=function(){
     })
     // #endregion turning card end
     
+
+
 }
+
+
+        // scrolling animation
+        window.addEventListener('scroll', () => {
+            const sections = document.querySelectorAll('.section');
+            sections.forEach(section => {
+                const rect = section.getBoundingClientRect();
+                if (rect.top < window.innerHeight * 0.85) {
+                    section.classList.add('visible');
+                }
+            });
+        });
+        
